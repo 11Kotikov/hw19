b65348b89f7beb6f30014698517de29c41de9341
Kotikov <11deadsluts@gmail.com>
2022-12-29 22:03:00 +0300

    task 3 is solved

--- hw19.py
+++ hw19.py
@@ -1,5 +1,5 @@
 from math import e
-from my_func import checking_number
+from my_func import *
 # Задача 1. Вычислить число c заданной точностью d
 # Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
@@ -12,27 +12,39 @@ from my_func import checking_number
 # Задача 2. Задайте натуральное число N.
 # Напишите программу, которая составит список простых множителей числа N.
 
-def prime_factors_list (n):
-    multipliers = []
-    count_mult = 0
-    i = 2
-    so_prime = n
-    while i<=n:
-        if n % i == 0:
-            multipliers.append(i)
-            n//=i
-            i = 2
-            count_mult +=1
-        else: 
-            i+=1
-    if count_mult <= 1:
-        multipliers = [1,so_prime]
-        return print(f'"{so_prime}" is a prime number, it divides only {multipliers}')
-    else:
-        multipliers.append(so_prime)
-        multipliers.insert(0,1)
-        return print (f'Your number was {so_prime} and its prime factors are {multipliers}')
+# def prime_factors_list (n):
+#     multipliers = []
+#     count_mult = 0
+#     i = 2
+#     so_prime = n
+#     while i<=n:
+#         if n % i == 0:
+#             multipliers.append(i)
+#             n//=i
+#             i = 2
+#             count_mult +=1
+#         else: 
+#             i+=1
+#     if count_mult <= 1:
+#         multipliers = [1,so_prime]
+#         return print(f'"{so_prime}" is a prime number, it divides only {multipliers}')
+#     else:
+#         multipliers.append(so_prime)
+#         multipliers.insert(0,1)
+#         return print (f'Your number was "{so_prime}" and its prime factors are {multipliers}')
         
-user_input = checking_number(input('Input possitive and integer number\
- to find out its prime factors: '))
-prime_factors_list (user_input)
+# user_input = checking_number(input('Input possitive and integer number\
+#  to find out its prime factors: '))
+# prime_factors_list (user_input)
+
+# Задача 3. Задайте последовательность чисел. 
+# Напишите программу, которая выведет список неповторяющихся элементов 
+# исходной последовательности.
+
+size_list = int(input("Please, input quantity of elements: "))
+print ("Now, input some integer numbers, don't even\
+ try to use the same ones, I'll delete it anyway  ")
+new_list = create_your_own_list(size_list)
+print (f'Your own list was: {new_list}')
+new_set = list(set(new_list))
+print (f'Now your list without the same numbers {new_set}')
